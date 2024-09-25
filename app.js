93594d3a955a5a7e6ed5c6db6b60dfd4e08048af
xeyzo <schneidergw@yahoo.com>
2024-09-25 12:23:44 +0700

    Feat : add new feat delete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 import fs from 'fs';
 import bodyParser from 'body-parser';
 
+
+var path = './store/todos.json'
 const app = express();
 const port = 3000;
 
@@ -10,15 +12,14 @@ const port = 3000;
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-app.get("/", (req,res) => {
-    res.send({message : "Hello World"});
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World" });
 });
 
-app.get('/todos', (req,res)=> {
-    fs.readFile('./store/todos.json','utf-8',(err, data)=>{
+app.get('/todos', (req, res) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
         if (!err) {
             const todos = JSON.parse(data);
-            console.log(todos)
 
             return res.send({
                 message: "Your data succesfully loaded",
@@ -27,29 +28,92 @@ app.get('/todos', (req,res)=> {
             }).status(200)
         }
 
-        return res.send({message:"Something went wrong"}).status(404)
+        return res.send({ message: "Something went wrong" }).status(404)
     })
 });
 
-app.get('/todos/:id', (req,res)=> {
-    fs.readFile('./store/todos.json','utf-8',(err, data)=>{
+app.get('/todos/:id', (req, res) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
         if (!err) {
             const todos = JSON.parse(data);
 
             const filterData = todos.findIndex(data => data.id == req.params.id)
             console.log(filterData)
 
+            if (filterData < 0) {
+                return res.send({ message: `data id ${req.params.id} not found` }).status(404)
+            }
+
             return res.send({
                 message: "Your data succesfully loaded",
-                data: [todos[filterData]],
+                data: todos[filterData],
                 status: 200
             }).status(200)
         }
 
-        return res.send({message:"Not found"}).status(404)
+        return res.send({ message: "Not found" }).status(404)
     })
 });
 
-app.listen(port, ()=> {
+app.post('/todos', (req, res) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if (!err) {
+            const todos = JSON.parse(data);
+            todos.push(req.body);
+
+            fs.writeFile(path, JSON.stringify(todos), 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing file', err);
+                } else {
+                    console.log('File has been updated successfully');
+                }
+            });
+
+            return res.send({
+                message: "Adding new data succesfully",
+                data: req.body,
+                status: 200
+            }).status(200)
+        }
+
+        return res.send({ message: "Something went wrong" })
+    })
+})
+
+app.delete('/todos/:id', (req, res) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+
+        if (err) {
+            return res.send({
+                message: "Something Went Wrong"
+            })
+        };
+        const todos = JSON.parse(data);
+
+        const findData = todos.findIndex(data => data.id == req.params.id)
+
+        if (findData < 0) {
+            return res.send({ message: `data id ${req.params.id} not found` }).status(404)
+        }
+
+        const result = todos.filter(item => item.id != req.params.id);
+
+        fs.writeFile(path, JSON.stringify(result), 'utf-8', (err) => {
+            if (err) {
+                console.error('Error writing file', err);
+            } else {
+                console.log('File has been updated successfully');
+            }
+        })
+
+        return res.send({
+            message: `data id ${req.params.id} has been deleted`
+        })
+    })
+})
+
+
+
+app.listen(port, () => {
     console.log(`your application ready on port http://localhost:${port}`)
 })
