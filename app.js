a5cc5537a859da9592fcb6a3dc0c767c7aad813d
xeyzo <schneidergw@yahoo.com>
2024-09-26 21:57:03 +0700

    feat: remove fs in project

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ import morgan from 'morgan';
 import fs from 'fs';
 import bodyParser from 'body-parser';
 
+const baseData = [
+    {
+        "id": 1,
+        "title": "todo example 1"
+    },
+    {
+        "id": 2,
+        "title": "todo example 2"
+    },
+    {
+        "id": 3,
+        "title": "todo example 3"
+    }
+];
 
-var path = './store/todos.json'
 const app = express();
 const port = 3000;
 
@@ -17,115 +30,76 @@ app.get("/", (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (!err) {
-            const todos = JSON.parse(data);
 
+        if (!baseData) {
             return res.send({
-                message: "Your data succesfully loaded",
-                data: todos,
-                status: 200
-            }).status(200)
+                message: "data not found"
+            })
         }
 
-        return res.send({ message: "Something went wrong" }).status(404)
-    })
+        return res.send({
+            message: "Your data succesfully loaded",
+            data: baseData,
+            status: 200
+        }).status(200)
+
+    return res.send({ message: "Something went wrong" }).status(404)
 });
 
 app.get('/todos/:id', (req, res) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (!err) {
-            const todos = JSON.parse(data);
+        const filterData = baseData.findIndex(data => data.id == req.params.id);
 
-            const filterData = todos.findIndex(data => data.id == req.params.id)
-            console.log(filterData)
-
-            if (filterData < 0) {
-                return res.send({ message: `data id ${req.params.id} not found` }).status(404)
-            }
-
-            return res.send({
-                message: "Your data succesfully loaded",
-                data: todos[filterData],
-                status: 200
-            }).status(200)
-        }
-
-        return res.send({ message: "Not found" }).status(404)
-    })
-});
-
-app.post('/todos', (req, res) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-        if (!err) {
-            const todos = JSON.parse(data);
-
-            const findMaxId = Math.max.apply(Math, todos.map(t => { return t.id }));
-            const generatedId = findMaxId + 1;
-
-            if (!req.body.title) {
-                return res.send({
-                    message: "Title mandatory"
-                })
-            } else {
-                todos.push({
-                    id: generatedId,
-                    title: `${req.body.title} ${generatedId}`
-                });
-    
-                fs.writeFile(path, JSON.stringify(todos), 'utf8', (err) => {
-                    if (err) {
-                        console.error('Error writing file', err);
-                    } else {
-                        console.log('File has been updated successfully');
-                    }
-                });
-            }
-
-            const responseData = {  id: generatedId, title: `${req.body.title} ${generatedId}`}
-
-            return res.send({
-                message: "Adding new data succesfully",
-                data: responseData,
-                status: 200
-            }).status(200);
-        }
-
-        return res.send({ message: "Something went wrong" });
-    })
-})
-
-app.delete('/todos/:id', (req, res) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-
-        if (err) {
-            return res.send({
-                message: "Something Went Wrong"
-            })
-        };
-
-        const todos = JSON.parse(data);
-
-        const findData = todos.findIndex(data => data.id == req.params.id);
-
-        if (findData < 0) {
+        if (filterData < 0) {
             return res.send({ message: `data id ${req.params.id} not found` }).status(404)
         };
 
-        const result = todos.filter(item => item.id != req.params.id);
-
-        fs.writeFile(path, JSON.stringify(result), 'utf-8', (err) => {
-            if (err) {
-                console.error('Error writing file', err);
-            } else {
-                console.log('File has been updated successfully');
-            }
-        });
-
         return res.send({
-            message: `data id ${req.params.id} has been deleted`
-        });
+            message: "Your data succesfully loaded",
+            data: baseData[filterData],
+            status: 200
+        }).status(200);
     });
+
+app.post('/todos', (req, res) => {
+
+    const findMaxId = Math.max.apply(Math, baseData.map(t => { return t.id }));
+    const generatedId = findMaxId + 1;
+
+    if (!req.body.title) {
+        return res.send({
+            message: "Title mandatory"
+        })
+    } else {
+        baseData.push({
+            id: generatedId,
+            title: `${req.body.title} ${generatedId}`
+        });
+    }
+
+    const responseData = { id: generatedId, title: `${req.body.title} ${generatedId}` }
+
+    return res.send({
+        message: "Adding new data succesfully",
+        data: responseData,
+        status: 200
+    }).status(200);
+})
+
+app.delete('/todos/:id', (req, res) => {
+
+    const findData = baseData.findIndex(data => data.id == req.params.id);
+
+    if (findData < 0) {
+        return res.send({ message: `data id ${req.params.id} not found` }).status(404)
+    };
+
+    const result = baseData.filter(item => item.id != req.params.id);
+
+    return res.send({
+        message: `data id ${req.params.id} has been deleted`,
+        data: result,
+        status: 200
+    }).status(200);
 });
 
 
